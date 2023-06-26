e4d65bece9cc27b7920abd3e83bdcd079daedc31
mjunaid106 <mjunaid84@gmail.com>
2023-06-26 22:46:17 +0100

    refactored

--- Order.API/Actors/IOrderActor.cs
+++ Order.API/Actors/IOrderActor.cs
@@ -4,7 +4,7 @@ namespace Order.API.Actors
 {
     public interface IOrderActor : IActor
     {
-        Task<Guid> SubmitAsync(Buyer buyer, IEnumerable<OrderItem> products);
+        Task<Guid> SubmitAsync(Buyer buyer, IList<OrderItem> products);
         Task<Order> GetDetails();
     }
 }
--- Order.API/Actors/OrderActor.cs
+++ Order.API/Actors/OrderActor.cs
@@ -8,28 +8,22 @@ namespace Order.API.Actors
     public class OrderActor : Actor, IOrderActor
     {
         private const string OrderDetailsStateName = "OrderDetails";
-        //private const string OrderStatusStateName = "OrderStatus";
-
-        private Guid OrderId => Guid.Parse(Id.GetId());
         DaprClient client;
 
         public OrderActor(ActorHost host) : base(host)
         {
             client = new DaprClientBuilder().Build();
         }
+        //private const string OrderStatusStateName = "OrderStatus";
 
-        protected override Task OnActivateAsync()
-        {
-            Console.WriteLine($"Activating actor id: {this.Id}");
-            return Task.CompletedTask;
-        }
+        private Guid OrderId => Guid.Parse(Id.GetId());
 
         public Task<Order> GetDetails()
         {
             return StateManager.GetStateAsync<Order>(OrderDetailsStateName);
         }
 
-        public async Task<Guid> SubmitAsync(Buyer buyer, IEnumerable<OrderItem> orderItems)
+        public async Task<Guid> SubmitAsync(Buyer buyer, IList<OrderItem> orderItems)
         {
             var order = new Order
             {
@@ -41,10 +35,19 @@ namespace Order.API.Actors
             };
 
             var request = client.CreateInvokeMethodRequest(HttpMethod.Get, "product-api", $"/api/Product/{orderItems.First().Product.Id}");
-            var product = client.InvokeMethodAsync<Product>(request);
+            //var product = await client.InvokeMethodAsync<Product>(request);
+            //if (product.QuantityAvailable + orderItems.First().Quantity >= 0)
+            {
+                await StateManager.SetStateAsync(OrderDetailsStateName, order);
+                return OrderId;
+            }
+            return Guid.Empty;
+        }
 
-            await StateManager.SetStateAsync(OrderDetailsStateName, order);
-            return OrderId;
+        protected override Task OnActivateAsync()
+        {
+            Console.WriteLine($"Activating actor id: {this.Id}");
+            return Task.CompletedTask;
         }
     }
 }
