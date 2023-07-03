f32b1e3c6e04b68d1104c7c930724890b5324a8c
mjunaid106 <mjunaid@live.com>
2023-07-03 18:47:46 +0100

    Add full name for the schema

--- Product.API/Program.cs
+++ Product.API/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers().AddDapr();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config => config.CustomSchemaIds(x => x.FullName));
 builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
